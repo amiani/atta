5a64a3b25abaaf3f16bd4b6778e82bbaa5ff631e
amiani <amiani@users.noreply.github.com>
2022-03-13 14:31:36 +0000

    deploy: 873fa8066e98f5f816645e16930f0e942e0b3c03

---
+++ _app/pages/__layout.svelte-9a84431f.js
@@ -0,0 +1,17 @@
+import{S as $,i as v,s as E,F as w,e as p,t as h,k as S,G as T,c as y,a as g,h as b,d as c,m as k,H as u,g as q,I as x,J as z,K as A,q as L,o as V}from"../chunks/vendor-568058bc.js";function Y(o){let s,d,n,i,l,r;const _=o[1].default,e=w(_,o,o[0],null);return{c(){s=p("style"),d=h(`@import url('https://fonts.googleapis.com/css2?family=Vollkorn:wght@400;500&display=swap');
+	`),n=p("style"),i=h(`* {
+			box-sizing: border-box;
+		}
+
+		body {
+			margin: 0;
+		}
+	`),l=S(),e&&e.c(),this.h()},l(t){const a=T('[data-svelte="svelte-p4cn0v"]',document.head);s=y(a,"STYLE",{});var f=g(s);d=b(f,`@import url('https://fonts.googleapis.com/css2?family=Vollkorn:wght@400;500&display=swap');
+	`),f.forEach(c),n=y(a,"STYLE",{});var m=g(n);i=b(m,`* {
+			box-sizing: border-box;
+		}
+
+		body {
+			margin: 0;
+		}
+	`),m.forEach(c),a.forEach(c),l=k(t),e&&e.l(t),this.h()},h(){document.title="ATTA"},m(t,a){u(document.head,s),u(s,d),u(document.head,n),u(n,i),q(t,l,a),e&&e.m(t,a),r=!0},p(t,[a]){e&&e.p&&(!r||a&1)&&x(e,_,t,t[0],r?A(_,t[0],a,null):z(t[0]),null)},i(t){r||(L(e,t),r=!0)},o(t){V(e,t),r=!1},d(t){c(s),c(n),t&&c(l),e&&e.d(t)}}}function C(o,s,d){let{$$slots:n={},$$scope:i}=s;return o.$$set=l=>{"$$scope"in l&&d(0,i=l.$$scope)},[i,n]}class G extends ${constructor(s){super();v(this,s,C,Y,E,{})}}export{G as default};
